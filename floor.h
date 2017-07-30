ac32f8f4469060077f7b2a33210d8452d82878ff
ulisesten <ulises.mtz.el@gmail.com>
2017-07-30 18:00:24 -0500

    Funciones de tiiempo

---
+++ floor.h
@@ -0,0 +1,13 @@
+#ifndef FLOORH //Si no esta definido floor.h
+#define FLOORH //Definirlo
+
+//declaraciones
+#include <stdio.h>
+#include <stdlib.h>
+
+//funciones
+void altura(int piso[]);//Coors altura
+
+//variables
+
+#endif //Termina definición
