a1b76b327ec498e735f89fff6749c477c9b57ac4
ulisesten <ulises.mtz.el@gamil.com>
2021-02-23 20:09:05 -0600

    update dep installation script

--- install_sdl.sh
+++ install_sdl.sh
@@ -4,3 +4,12 @@ sudo apt install -y libsdl2-image-dev
 sudo apt install -y libsdl2-mixer-dev
 sudo apt install -y zlib1g-dev
 sudo apt install -y libpng-dev
+
+if [ $? -ne 0 ]; then
+echo "[ERROR]No se instalaron correctamente las dependencias"
+else
+echo "Se instalaron correctamente las dependencias"
+fi
+
+echo "Ha terminado la instalacion de SDL2. Consulte el archivo README.md para mas informacion."
+#zenity --info --title="MetalSlug" --text=
